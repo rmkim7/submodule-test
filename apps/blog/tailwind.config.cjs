a62422cfd1083c5cb7d6840e9c301e8c4ea6d163
rmkim7 <rmjkim7@gmail.com>
2025-01-11 22:24:05 +0900

    chore: tailwind css 관련 config 수정

---
+++ apps/blog/tailwind.config.cjs
@@ -0,0 +1,11 @@
+const path = require("path");
+/** @type {import('tailwindcss').Config} */
+const config = require("@repo/tailwind-config/tailwindConfig");
+
+module.exports = {
+  ...config,
+  content: [
+    path.resolve(__dirname, "./app/**/*.{tsx,jsx,ts,js,mdx}"),
+    path.resolve(__dirname, "../../packages/ui/**/*.{tsx,jsx,ts,js,mdx}"),
+  ],
+};
