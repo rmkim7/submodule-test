60f5a1625e4a222af450a9041f2e1b0b9df58b95
rmkim7 <rmjkim7@gmail.com>
2025-01-26 04:14:19 +0900

    feat: 게시글 작성 기능 추가

--- apps/blog/apis/posts.ts
+++ apps/blog/apis/posts.ts
@@ -1,4 +1,4 @@
-interface Post {
+export interface Post {
   id: string;
   title: string;
   content: string;
@@ -10,10 +10,11 @@ interface Post {
 type CreatePostDTO = Pick<Post, "title" | "content" | "author">;
 type UpdatePostDTO = Partial<CreatePostDTO>;
 
-const baseUrl = "http://localhost:3000/api";
+const BASE_URL = "http://localhost:3000/api";
 
+// 모든 게시글 조회
 export const fetchPosts = async (): Promise<Post[]> => {
-  const response = await fetch(`${baseUrl}/posts`, {
+  const response = await fetch(`${BASE_URL}/posts`, {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
@@ -22,5 +23,42 @@ export const fetchPosts = async (): Promise<Post[]> => {
   if (!response.ok) {
     throw new Error(`${response.status} ${response.statusText}`);
   }
-  return response.json() as Promise<Post[]>;
+  return response.json();
 };
+
+// 새 게시글 작성
+export const createPost = async (post: CreatePostDTO): Promise<Post> => {
+  const response = await fetch(`${BASE_URL}/posts`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      title: post.title,
+      content: post.content,
+      author: post.author,
+    }),
+  });
+  if (!response.ok) {
+    throw new Error(`${response.status} ${response.statusText}`);
+  }
+  return response.json();
+};
+
+// 특정 게시글 조회
+export const searchPost = async (id: string): Promise<Post> => {
+  const response = await fetch(`${BASE_URL}/posts/${id}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  if (!response.ok) {
+    throw new Error(`${response.status} ${response.statusText}`);
+  }
+  return response.json();
+};
+
+// 게시글 수정
+
+// 게시글 삭제
---
+++ apps/blog/app/posts/write/page.tsx
@@ -0,0 +1,64 @@
+import { createPost } from "@/apis/posts";
+import Link from "next/link";
+import Button from "@repo/ui/components/button";
+import Form from "next/form";
+import { redirect } from "next/navigation";
+
+export default function PostMakingPage() {
+  async function createPostAction(formData: FormData) {
+    "use server";
+    const title = formData.get("title") as string;
+    const content = formData.get("content") as string;
+    const author = formData.get("author") as string;
+
+    const postDetail = await createPost({ title, content, author });
+
+    const postId = postDetail.id;
+
+    redirect(`/posts/${postId}`);
+  }
+
+  return (
+    <>
+      <Button
+        divClassName="text-right mr-8"
+        buttonClassName="border border-gray-400 bg-white text-black font-semibold text-base p-2 rounded"
+      >
+        <Link href={`/`}>나가기</Link>
+      </Button>
+
+      <Form action={createPostAction}>
+        <div className="border border-gray-400 mx-8 my-8 rounded p-2 grid">
+          <input
+            name="title"
+            required={true}
+            placeholder="제목을 입력하세요"
+            className="border border-gray-400 mx-2 my-3 rounded p-2 overflow-auto"
+            type="text"
+          />
+          <hr className="mx-1" />
+          <textarea
+            name="content"
+            required={true}
+            placeholder="내용을 입력하세요"
+            rows={1}
+            className="border border-gray-400 mx-2 my-3 rounded p-2 min-h-40 overflow-auto"
+          />
+          <input
+            name="author"
+            required={true}
+            placeholder="작성자"
+            maxLength={30}
+            className="border border-gray-300 bg-gray-300 mx-2 mb-2 p-0.5 rounded italic"
+          />
+        </div>
+        <Button
+          divClassName="text-right mr-8 mt-4"
+          buttonClassName="bg-black text-white font-semibold text-base p-2 rounded-lg"
+        >
+          완료
+        </Button>
+      </Form>
+    </>
+  );
+}
