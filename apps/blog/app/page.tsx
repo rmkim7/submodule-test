c88d48ef7e4f0b1e7231a3664d28407d24a6a8aa
rmkim7 <rmjkim7@gmail.com>
2025-01-26 04:12:20 +0900

    refactor: css 스타일 수정 및 변수명 변경

--- apps/blog/app/page.tsx
+++ apps/blog/app/page.tsx
@@ -3,20 +3,42 @@ import Link from "next/link";
 import Button from "@repo/ui/components/button";
 
 export default async function Home() {
-  const fetchList = await fetchPosts();
-  console.log(fetchList);
-  console.log(fetchList[0].title);
+  const postList = await fetchPosts();
+  console.log(postList);
 
   return (
     <>
       <Button
         divClassName="text-right mr-8"
-        buttonClassName="bg-black text-white font-semibold text-base p-3 rounded-lg"
+        buttonClassName="bg-black text-white font-semibold text-base p-2 rounded"
       >
         <Link href="/posts/write">새 글 작성</Link>
       </Button>
+      <h2 className="mx-8 mt-8 mb-8 font-semibold">목록</h2>
+      {postList.map(({ id, title, content, author, createdAt }) => {
+        const date = new Date(createdAt);
+        const localCreatedAt = date.toLocaleString("ko-KR");
 
-      <div className="border-gray-400 w-80 h-96"></div>
+        return (
+          <div
+            key={id}
+            className="border border-gray-400 mx-8 my-4 rounded p-4"
+          >
+            <Link href={`/posts/${id}`}>
+              <h3 className="font-semibold pb-4">{title}</h3>
+              <hr />
+              <p className="pt-4">{content}</p>
+              <br></br>
+              <div className="flex flex-col gap-2">
+                <span className="border border-gray-300 bg-gray-300 px-2 py-0.5 rounded italic">
+                  {author}
+                </span>
+                <time>{localCreatedAt}</time>
+              </div>
+            </Link>
+          </div>
+        );
+      })}
     </>
   );
 }
